bc5c2c5385f10fafd3464904f88e2c07ca45d2f4
Daviicode <d.ydzdavid@yahoo.com>
2019-07-12 13:22:29 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector() {
+  return document.querySelectorAll('p')
+}//for alln p tag
+
+function lastImageSelector() {
+  return $('img:last')
+}//
+
+// function ninjaBabaySelector() {
+//   return $('#baby-ninja')
+// }//
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return document.querySelectorAll('.pics')
+}//. for class name select
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
